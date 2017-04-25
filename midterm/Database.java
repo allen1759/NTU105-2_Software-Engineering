80cf6f4e7065c92a30553419c6aadb00db9e2422
b02902048 <b02902048@linux7.csie.ntu.edu.tw>
2017-04-25 15:57:53 +0800

    Modify Database.java

--- midterm/Database.java
+++ midterm/Database.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 public class Database{
 
-	public List<String> inputOrder;
-	public Map<String, HashMap<ArrayList<Record>>> patientRecord;
+	public List<String> patientOrder;
+	public List<String> deviceOrder;
+	public HashMap<String, HashMap<String, ArrayList<Record>>> patientRecord;
 
 	Database(){
-		inputOrder = new ArrayList<String>();
-		patientRecord = new HashMap<String, HashMap<ArrayList<Record>>>();
+		patientOrder = new ArrayList<String>();
+		deviceOrder = new ArrayList<String>();
+		patientRecord = new HashMap<String, HashMap<String, ArrayList<Record>>>();
 	}
 
 	public void insert(String patientName, String deviceName, int time, float value){
-		if(!patientRecord.containskey(patientName)){
-			inputOrder.add(patientName);
-			Map<ArrayList<Record>> recordList = new HashMap<ArrayList<Record>>();
+		if(!patientRecord.containsKey(patientName)){
+			patientOrder.add(patientName);
+			HashMap<String, ArrayList<Record>> recordList = new HashMap<String, ArrayList<Record>>();
 			patientRecord.put(patientName, recordList);
 		}
-		if(!patientRecord.get(patientName).containskey(deviceName)){
+		if(!patientRecord.get(patientName).containsKey(deviceName)){
 			ArrayList<Record> record = new ArrayList<Record>();
+			deviceOrder.add(deviceName);
 			patientRecord.get(patientName).put(deviceName, record);
 		}
 
@@ -26,7 +29,15 @@ public class Database{
 	}
 
 	public void display(){
-		
+		for(String patientName: inputOrder){
+			System.out.println("patient "+patientName);
+			for(String deviceName: deviceOrder){
+				System.out.println(deviceName);
+				for(ArrayList<Record> record: patientRecord.get(patientName).get(deviceName)){
+					System.out.println("["+record.time+"] "+record.value);
+				}
+			}
+		}
 	}
 
 	private class Record{
