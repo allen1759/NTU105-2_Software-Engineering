da7cfe3023b829390a849cae8bb67f797c855a38
davidchen0310 <b03705028@ntu.edu.tw>
2017-04-25 16:21:42 +0800

    revise patient

--- midterm/Patient.java
+++ midterm/Patient.java
@@ -13,6 +13,10 @@ public class Patient {
 	public String getName(){
 		return name;
 	}
+
+	public Integer getFrequency(){
+		return frequency;
+	}
 	
 	public Boolean attachAnalogDevice(String[] deviceInformation){
 
@@ -45,30 +49,5 @@ public class Patient {
 			res.add(deviceMeasurement);
 		}
 		return res;
-	}
-
-	public class DeviceMeasurement{
-		private String deviceCategory;
-		private String deviceName;
-		private Float measurementValue;
-
-
-		public DeviceMeasurement(String deviceCategory, String deviceName, Float measurementValue){
-			this.deviceCategory = deviceCategory;
-			this.deviceName = deviceName;
-			this.measurementValue = measurementValue;
-		}
-
-		public String getDeviceCategory(){
-			return deviceCategory;
-		}
-
-		public String getDeviceName(){
-			return deviceName;
-		}
-
-		public Float getmeasurementValue(){
-			return measurementValue;
-		}
 	}
 }
