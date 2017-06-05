391cd24b6624ce6b2fc54de64e046ee80288f2fa
Allen Yang <allen1759@gmail.com>
2017-06-05 23:59:23 +0800

    Add Week15HW1 code.

---
+++ HW/Week15HW1/Allen/Document.java
@@ -0,0 +1,22 @@
+
+public class Document {
+
+	private String content;
+	
+	public Document() {
+		content = new String();
+	}
+	
+	public void Insert(String str) {
+		// add str into content
+	}
+	
+	public void Delete() {
+		// delete
+	}
+	
+	public String GetSelect() {
+		// return the selected string
+		return new String();
+	}
+}
